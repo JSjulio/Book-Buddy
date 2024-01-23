4af010a4c48a84a555c3c59d7ed869938a4f9887
Hisashi Peterson <146689382+sashipeterson@users.noreply.github.com>
2024-01-23 09:49:12 +0900

    Added login onto app page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import bookLogo from './assets/books.png'
 import Books from './components/Books'
 import Navigation from './components/Navigations'
+import Login from './components/Login'
 
 function App() {
   const [token, setToken] = useState(null)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <h1><img id='logo-image' src={bookLogo}/>Library App</h1>
+      <Login />
       <Navigation />
       <Books />
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,1 +1,8 @@
 /* TODO - add your code to create a functional React component that renders a login form */
+const Login = () => {
+    return (
+        <p>Login</p>
+    )
+};
+
+export default Login
