1088794fe928b0d7ba073fd7960dea935b5228bc
Isaias Julio <isaiasjulio@Isaiass-MacBook-Air-2.local>
2024-01-23 00:53:06 -0500

    set up store/ created bookApi / created features folder

--- app/store.js
+++ app/store.js
@@ -1,1 +1,15 @@
-import { configureStore } from "reduxjs/toolkit"; 
+import { configureStore } from "@reduxjs/toolkit"
+import { bookApi } from "../api/bookApi"
+// to install the toolkit run the: 
+// npm install @reduxjs/toolkit react-redux
+
+export const store = configureStore({ 
+    // TODO reducer goes here 
+
+
+    // TODO  middleware goes here
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(bookApi.middleware),
+});
+
+
