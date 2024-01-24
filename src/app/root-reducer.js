83f9d01cc71b67222ab713509b70f87fdde72e1f
Hisashi Peterson <146689382+sashipeterson@users.noreply.github.com>
2024-01-25 08:31:20 +0900

    Succesfully rendered API call for books, configured so that state is managed by store in line with redux

--- src/app/root-reducer.js
+++ src/app/root-reducer.js
@@ -1,5 +1,8 @@
-import { combineReducers } from "redux"
+import BooksReducer from '../components/BooksSlice';
+import { combineReducers } from 'redux';
 
+//Combine reducers
+//If you create another reducer place in here
 export const rootReducer = combineReducers ({
-    
+    books: BooksReducer
 })
--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./root-reducer";
 
+//All reducers are combined in root reducer so you don't need to make any change to the sotry
 export const store = configureStore ({
-    reducer: rootReducer
+    reducer: rootReducer,
 })
