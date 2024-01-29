abab862f7ad8a4d6d2217959f5b6ef29e0c2be9a
Hisashi Peterson <146689382+sashipeterson@users.noreply.github.com>
2024-01-29 13:34:55 +0900

    fixed merge errors

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import { useSelector, useDispatch } from 'react-redux';
 
 import { useState } from 'react'
 import bookLogo from './assets/books.png'
-import Books from './components/Books'
+
 import Navigation from './components/Navigations'
-import Login from './components/Login'
+
 
 
 //Nav Elements
